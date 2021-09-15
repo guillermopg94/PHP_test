4080d9f4791020316e6399535ec75908ee0a762d
guipagra <83446487+guillermopg94@users.noreply.github.com>
2021-09-15 08:55:07 +0200

    Update primerphp.php

--- practica13/primerphp.php
+++ practica13/primerphp.php
@@ -15,5 +15,17 @@ $apellido=Paton;
 ?>
     <p> <a href="segundophp.php?nombre=guille&apellido=paton">segundo enlace sin variable </a> </p>
     <p> <a href="segundophp.php?nombre=<?php echo $nombre;?>&apellido=<?php echo $apellido;?>">segundo enlace con variable</a> </p>
+    <p> <a href="primerphp.php?nombre=<?php echo $nombre;?>&apellido=<?php echo $apellido;?>">enlace isset</a> </p>
+
+    <?php
+    if (isset($_GET["nombre"])){
+        //Recoger las variables nombre y apellidos con $_GET
+        echo $_GET["nombre"];
+    } else {
+        echo "Aun no existe la variable nombre porque no has pulsado el enlace";
+    }
+    ?>
+
+
 </body>
 </html>
