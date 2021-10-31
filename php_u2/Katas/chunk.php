21cad09ed33b348a279563facac1a8717a2817c1
guipagra <83446487+guillermopg94@users.noreply.github.com>
2021-10-31 12:31:36 +0100

    Update chunk.php

--- php_u2/Katas/chunk.php
+++ php_u2/Katas/chunk.php
@@ -6,25 +6,24 @@ Ese array asociativo va a atener las siguientes claves y valores:
 Clave Material = Tierra
 Clave Herramienta = Pala
 Clave Brillo = 0
-
 Hacer una función llamada $herramienta = devuelveHerramienta($Mundo, 3, 4, 7); (4 parametros(El array Mundo y las tres coordenadas))
 que pasando como parametros el Mundo 
 y tres cordenadas nos tiene que devolver 
 la herramienta necesaria para picarlo.
  -->
+<?php
 
- <?php
+$Mundo = array();
 
-
-
-function devuelveHerramienta($mundo, $num1, $num2, $num3){
-    
-
-
+for ($i = 0; $i < 17; $i++) {
+  array_push($Mundo, array());
 }
+for ($i = 0; $i < 17; $i++) {
+  array_push($Mundo[$i], array("Material" => "Tierra",  "Herramienta" => "Pala",  "Brillo" => 0));
+}
+function devuelveHerramienta($mundo, $num1, $num2, $clave){
+  echo $mundo[$num1][$num2][$clave];
+}
+$Mundo[14][0] = array("Material" => "Tierra",  "Herramienta" => "Pico",  "Brillo" => 0);
 
-
- $herramienta = devuelveHerramienta($Mundo, 3, 4, 7); 
- echo($herramienta);
- 
- ?>
+devuelveHerramienta($Mundo, 14, 0, "Herramienta");
